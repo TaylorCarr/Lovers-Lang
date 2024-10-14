a7242be09dbfd0650630e83bff3a2fde4046bbf3
Taylor Carr <tayloracarr94@gmail.com>
2024-10-14 15:05:21 -0500

    testing lambda

--- AWS/src/index.mjs
+++ AWS/src/index.mjs
@@ -1,8 +1,13 @@
 export const handler = async (event) => {
+  let headers = event.headers
+  let body = JSON.parse(event.body)
+  let message = body.message
+
     // TODO implement
     const response = {
       statusCode: 200,
-      body: JSON.stringify('Hello from Lambda!'),
+      headers: headers,
+      body: JSON.stringify(message),
     };
     return response;
   };
